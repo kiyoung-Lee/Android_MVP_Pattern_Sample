23b66b1c08b864d7b3b157826592aa8062b9dfd1
kiyoung Lee <eevee300@gmail.com>
2017-06-27 21:56:12 +0900

    firebase - facebook 연동진행중

--- app/src/main/java/com/example/mvp_sample/Common/SampleApplication.java
+++ app/src/main/java/com/example/mvp_sample/Common/SampleApplication.java
@@ -2,6 +2,8 @@ package com.example.mvp_sample.Common;
 
 import android.app.Application;
 
+import com.facebook.login.LoginManager;
+
 /**
  * Created by KiyoungLee on 2017-06-17.
  */
@@ -11,5 +13,11 @@ public class SampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        LoginManager.getInstance().logOut();
     }
 }
--- app/src/main/java/com/example/mvp_sample/Login/LoginActivity.java
+++ app/src/main/java/com/example/mvp_sample/Login/LoginActivity.java
@@ -2,16 +2,28 @@ package com.example.mvp_sample.Login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.mvp_sample.Login.Data.LoginRepositoryImpl;
 import com.example.mvp_sample.R;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.ButterKnife;
 
@@ -23,6 +35,8 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Ac
 
     private LoginContract.Presenter presenter;
     private CallbackManager callbackManager;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,6 +46,7 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Ac
 
         presenter = new LoginPresenterImpl(new LoginRepositoryImpl());
         presenter.setActivityView(this);
+        mAuth = FirebaseAuth.getInstance();
 
         callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = (LoginButton) ButterKnife.findById(this, R.id.login_button);
@@ -43,6 +58,7 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Ac
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
+                handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
             @Override
@@ -55,6 +71,48 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Ac
                 // App code
             }
         });
+
+//        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//
+//            }
+//
+//            @Override
+//            public void onError(FacebookException error) {
+//
+//            }
+//        });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                } else {
+                    // User is signed out
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
@@ -66,5 +124,24 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Ac
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
