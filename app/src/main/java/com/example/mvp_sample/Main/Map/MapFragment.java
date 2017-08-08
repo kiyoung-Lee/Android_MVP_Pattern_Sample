d84652c8f6344ba68f51e1b890d20994f6a0b1e0
kiyoung Lee <eevee300@gmail.com>
2017-08-08 22:22:37 +0900

    naver 지도 API 튜토리얼 구현체 추가

--- app/src/main/java/com/example/mvp_sample/Main/Map/MapFragment.java
+++ app/src/main/java/com/example/mvp_sample/Main/Map/MapFragment.java
@@ -9,6 +9,8 @@ import android.view.ViewGroup;
 
 
 import com.example.mvp_sample.R;
+import com.nhn.android.maps.NMapContext;
+import com.nhn.android.maps.NMapView;
 
 import butterknife.ButterKnife;
 
@@ -18,6 +20,8 @@ import butterknife.ButterKnife;
 
 public class MapFragment extends Fragment implements MapContract.FragmentView {
 
+    private NMapContext mMapContext;
+    private static final String CLIENT_ID = "YOUR_CLIENT_ID";// 애플리케이션 클라이언트 아이디 값
 
     public MapFragment() {
     }
@@ -29,6 +33,49 @@ public class MapFragment extends Fragment implements MapContract.FragmentView {
         View view = inflater.inflate(R.layout.map_frag, container, false);
         ButterKnife.bind(getActivity());
         return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMapContext =  new NMapContext(super.getActivity());
+        mMapContext.onCreate();
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        NMapView mapView = (NMapView)getView().findViewById(R.id.mapView);
+        mapView.setClientId(CLIENT_ID);// 클라이언트 아이디 설정
+        mMapContext.setupMapView(mapView);
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        mMapContext.onStart();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapContext.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapContext.onPause();
+    }
+    @Override
+    public void onStop() {
+        mMapContext.onStop();
+        super.onStop();
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+    @Override
+    public void onDestroy() {
+        mMapContext.onDestroy();
+        super.onDestroy();
     }
 
     @Override
