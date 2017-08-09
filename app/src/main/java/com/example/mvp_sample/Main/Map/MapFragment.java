a31f01263a076bd529a00781fc2b69b01bcb30c5
kiyoung Lee <eevee300@gmail.com>
2017-08-09 22:01:01 +0900

    네이버 지도 -> 다음 지도 연동으로 변경

--- app/src/main/java/com/example/mvp_sample/Main/Map/MapFragment.java
+++ app/src/main/java/com/example/mvp_sample/Main/Map/MapFragment.java
@@ -9,8 +9,8 @@ import android.view.ViewGroup;
 
 
 import com.example.mvp_sample.R;
-import com.nhn.android.maps.NMapContext;
-import com.nhn.android.maps.NMapView;
+
+import net.daum.android.map.MapView;
 
 import butterknife.ButterKnife;
 
@@ -20,62 +20,19 @@ import butterknife.ButterKnife;
 
 public class MapFragment extends Fragment implements MapContract.FragmentView {
 
-    private NMapContext mMapContext;
-    private static final String CLIENT_ID = "YOUR_CLIENT_ID";// 애플리케이션 클라이언트 아이디 값
-
-    public MapFragment() {
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.map_frag, container, false);
         ButterKnife.bind(getActivity());
-        return view;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mMapContext =  new NMapContext(super.getActivity());
-        mMapContext.onCreate();
-    }
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        NMapView mapView = (NMapView)getView().findViewById(R.id.mapView);
-        mapView.setClientId(CLIENT_ID);// 클라이언트 아이디 설정
-        mMapContext.setupMapView(mapView);
-    }
-    @Override
-    public void onStart(){
-        super.onStart();
-        mMapContext.onStart();
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        mMapContext.onResume();
-    }
-    @Override
-    public void onPause() {
-        super.onPause();
-        mMapContext.onPause();
-    }
-    @Override
-    public void onStop() {
-        mMapContext.onStop();
-        super.onStop();
-    }
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-    }
-    @Override
-    public void onDestroy() {
-        mMapContext.onDestroy();
-        super.onDestroy();
+        MapView mapView = new MapView(getActivity());
+        ViewGroup mapViewContainer = (ViewGroup) ButterKnife.findById(getActivity(), R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        return view;
     }
 
     @Override
